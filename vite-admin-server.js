005aea6dd0a584726ab6efa038e30b397740c754
canyyang <canyangchen@126.com>
2024-12-25 17:07:34 +0800

    FIX server config

--- vite-admin-server.js
+++ vite-admin-server.js
@@ -10,13 +10,13 @@ const options = {
 
 // 启动 https 服务
 const app = server.start({
-  port: 7002,
+  port: 7003,
   directory: './dist'
 });
 
 // 使用 https.createServer 来包裹 app
 https.createServer(options, (req, res) => {
   app(req, res); // 将请求转发给 pushstate-server 的处理函数
-}).listen(7002, () => {
+}).listen(7003, () => {
   console.log('Server running at https://canyyang.xyz:7002');
 });
