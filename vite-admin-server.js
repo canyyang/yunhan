0c3514d01c5c01406bd280e6df20e6ad6afe7344
canyyang <canyangchen@126.com>
2024-12-25 17:17:34 +0800

    FIX server config

--- vite-admin-server.js
+++ vite-admin-server.js
@@ -1,5 +1,4 @@
 const server = require('pushstate-server');
-const https = require('https');
 const fs = require('fs');
 
 // 读取证书文件
@@ -8,15 +7,8 @@ const options = {
   cert: fs.readFileSync('/etc/letsencrypt/live/canyyang.xyz/fullchain.pem')
 };
 
-// 启动 https 服务
-const app = server.start({
-  port: 7003,
+server.start({
+  port: 7002,
   directory: './dist'
-});
+})
 
-// 使用 https.createServer 来包裹 app
-https.createServer(options, (req, res) => {
-  app(req, res); // 将请求转发给 pushstate-server 的处理函数
-}).listen(7003, () => {
-  console.log('Server running at https://canyyang.xyz:7002');
-});
