26dc062b2af15e2703bed32ecde1b6545e777231
canyyang <canyangchen@126.com>
2024-12-25 16:53:21 +0800

    FIX server config

--- vite-admin-server.js
+++ vite-admin-server.js
@@ -1,6 +1,19 @@
 const server = require('pushstate-server')
+const https = require('https')
+const fs = require('fs')
 
-server.start({
+// 读取证书文件
+const options = {
+  key: fs.readFileSync('/etc/letsencrypt/live/canyyang.xyz/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/canyyang.xyz/fullchain.pem')
+}
+
+// 启动 https 服务
+const app = server.start({
   port: 7002,
   directory: './dist'
+})
+
+https.createServer(options, app).listen(7002, () => {
+  console.log('Server running at https://canyyang.xyz:7002')
 })
