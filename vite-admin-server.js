f07636e58f5f0e51d99501b22d4590090f30c66f
canyyang <canyangchen@126.com>
2024-12-26 00:00:16 +0800

    FIX server config

--- vite-admin-server.js
+++ vite-admin-server.js
@@ -1,11 +1,11 @@
 const server = require('pushstate-server');
-const fs = require('fs');
+// const fs = require('fs');
 
 // 读取证书文件
-const options = {
-  key: fs.readFileSync('/etc/letsencrypt/live/canyyang.xyz/privkey.pem'),
-  cert: fs.readFileSync('/etc/letsencrypt/live/canyyang.xyz/fullchain.pem')
-};
+// const options = {
+//   key: fs.readFileSync('/etc/letsencrypt/live/canyyang.xyz/privkey.pem'),
+//   cert: fs.readFileSync('/etc/letsencrypt/live/canyyang.xyz/fullchain.pem')
+// };
 
 server.start({
   port: 7002,
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import ElementPlus from 'unplugin-element-plus/vite'
 import path from 'path'
-import fs from 'fs'
+// import fs from 'fs'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,12 +12,12 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src/')
     }
   },
-  server: {
-    host: '0.0.0.0',  // 监听所有网络接口
-    port: 7002,       // 配置监听端口
-    https: {
-      key: fs.readFileSync('/etc/letsencrypt/live/canyyang.xyz/privkey.pem'),
-      cert: fs.readFileSync('/etc/letsencrypt/live/canyyang.xyz/fullchain.pem'),
-    }
-  }
+  // server: {
+  //   host: '0.0.0.0',  // 监听所有网络接口
+  //   port: 7002,       // 配置监听端口
+  //   https: {
+  //     key: fs.readFileSync('/etc/letsencrypt/live/canyyang.xyz/privkey.pem'),
+  //     cert: fs.readFileSync('/etc/letsencrypt/live/canyyang.xyz/fullchain.pem'),
+  //   }
+  // }
 })
